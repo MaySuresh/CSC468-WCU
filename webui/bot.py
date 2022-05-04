1dbffcf095b199d5d81ca329a713a4b4fae26e66
MaySuresh <90007893+MaySuresh@users.noreply.github.com>
2022-05-03 21:51:52 -0400

    Add files via upload

---
+++ webui/bot.py
@@ -0,0 +1,64 @@
+# importing the module 
+
+from datetime import time
+import praw  
+
+# initialize with appropriate values 
+client_id = "6SVrWF9tph3rr3-r98E1VA" 
+client_secret = "Jx_HLO3mLeVTTHMkAjHMSmbZqG2WcA" 
+username = "SourceScanCSC468" 
+password = "Group8jamds" 
+user_agent = "SourceScan" 
+  
+# creating an authorized reddit instance 
+reddit = praw.Reddit(client_id = client_id,  
+                     client_secret = client_secret,  
+                     username = username,  
+                     password = password, 
+                     user_agent = user_agent) 
+                     
+# to find the top most submission in the subreddit "news" 
+subreddit = reddit.subreddit('news')
+print("r/news\n")
+for submission in subreddit.search('Russia', sort='top',time_filter='week', limit=3): 
+    # displays the submission title 
+    print(submission.title)   
+    # displays the net upvotes of the submission 
+    print(submission.score)  
+    # displays the url of the submission 
+    print(submission.url)
+
+subreddit = reddit.subreddit('worldnews')
+print("\nr/worldnews\n")
+for submission in subreddit.search('Russia', sort='top', time_filter='week', limit=3):
+        # displays the submission title 
+    print(submission.title)   
+        # displays the net upvotes of the submission 
+    print(submission.score)  
+        # displays the url of the submission 
+    print(submission.url)
+
+subreddit = reddit.subreddit('politics')
+print("\nr/politics\n")
+for submission in subreddit.search('Russia', sort='top', time_filter='week', limit=3):
+        # displays the submission title 
+    print(submission.title)   
+        # displays the net upvotes of the submission 
+    print(submission.score)  
+        # displays the url of the submission 
+    print(submission.url)
+
+subreddit = reddit.subreddit('technews')
+print("\nr/technews\n")
+for submission in subreddit.search('Russia', sort='top', time_filter='week', limit=3):
+        # displays the submission title 
+    print(submission.title)   
+        # displays the net upvotes of the submission 
+    print(submission.score)  
+        # displays the url of the submission 
+    print(submission.url)
+
+
+    
+
+       
